90aa18f61ebe202b2b46d9975ff69d048f7bc872
ynyanchoka <monaryymelda@gmail.com>
2022-07-04 11:29:35 +0300

    add changes

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,65 @@
+package org.example;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+
+        //home
+
+        get("/",(request, response) -> {
+            Map<String,Object> model=new HashMap<String, Object>();
+            return new ModelAndView(model,"login.hbs");
+        },new HandlebarsTemplateEngine());
+
+        //form admin
+        get( "/admin/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "login.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //form teacher
+        get("/login/new/teacher",(request, response) -> {
+            Map<String,Object> model=new HashMap<String, Object>();
+            return new ModelAndView(model,"login.hbs");
+        },new HandlebarsTemplateEngine());
+
+        //form register
+        get("/register/new",(request, response) -> {
+            Map<String,Object> model=new HashMap<String, Object>();
+            return new ModelAndView(model,"register.hbs");
+        },new HandlebarsTemplateEngine());
+
+        //form login
+        get("/login/new",(request, response) -> {
+            Map<String,Object> model=new HashMap<String, Object>();
+            return new ModelAndView(model,"login.hbs");
+        },new HandlebarsTemplateEngine());
+
+        //form resource
+        get("/resources/new",(request, response) -> {
+            Map<String,Object> model=new HashMap<String, Object>();
+            return new ModelAndView(model,"resources.hbs");
+        },new HandlebarsTemplateEngine());
+
+        //form resource
+        get("/attendance/new",(request, response) -> {
+            Map<String,Object> model=new HashMap<String, Object>();
+            return new ModelAndView(model,"attendance.hbs");
+        },new HandlebarsTemplateEngine());
+        get("/logout",(request, response) -> {
+            Map<String,Object> model=new HashMap<String, Object>();
+            return new ModelAndView(model,"login.hbs");
+        },new HandlebarsTemplateEngine());
+
+
+    }
+}
