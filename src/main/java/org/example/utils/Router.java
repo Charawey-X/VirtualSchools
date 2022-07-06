33fd1262736867e056136da747ad1eec3d1a60a4
chacha <chachaerickjo@gmail.com>
2022-07-06 11:14:15 +0300

    fx - fix broken routes; methods in the dao were not implemented right

--- src/main/java/org/example/utils/Router.java
+++ src/main/java/org/example/utils/Router.java
@@ -1,18 +1,9 @@
 package org.example.utils;
 
 import com.google.gson.Gson;
-import org.example.dao.SchoolDao;
-import org.example.dao.StudentDao;
-import org.example.dao.TeacherDao;
-import org.example.dao.UserDao;
-import org.example.interfaces.ISchool;
-import org.example.interfaces.IStudent;
-import org.example.interfaces.ITeacher;
-import org.example.interfaces.IUser;
-import org.example.models.School;
-import org.example.models.Student;
-import org.example.models.Teacher;
-import org.example.models.Users;
+import org.example.dao.*;
+import org.example.interfaces.*;
+import org.example.models.*;
 import org.sql2o.Connection;
 
 import java.util.List;
@@ -25,6 +16,7 @@ public class Router {
         ITeacher teacherDao = new TeacherDao(connection); //TODO: add teacher dao
         IStudent studentDao = new StudentDao(connection); //TODO: add student dao
         ISchool schoolDao = new SchoolDao(connection); //TODO: add school dao
+        ISubject subjectDao = new SubjectDao(connection); //TODO: add subject dao
 
         //TODO: add user routes
         post("/api/v1/users/login", (req, res) -> {
@@ -72,9 +64,9 @@ public class Router {
         post("/api/v1/teachers/register", (req, res) -> {
             Gson gson = new Gson();
             Teacher teacherData = gson.fromJson(req.body(), Teacher.class);
-            Teacher teacher = teacherDao.createTeacher(teacherData);
+            teacherDao.createTeacher(teacherData);
             res.type("application/json");
-            return gson.toJson(teacher);
+            return gson.toJson("Teacher created");
         });
 
         get("/api/v1/teachers/:id", (req, res) -> {
@@ -96,26 +88,26 @@ public class Router {
             Gson gson = new Gson();
             Teacher teacherData = gson.fromJson(req.body(), Teacher.class);
             teacherData.setId(Integer.parseInt(req.params(":id")));
-            Teacher teacher = teacherDao.updateTeacher(teacherData);
+            teacherDao.updateTeacher(teacherData);
             res.type("application/json");
-            return gson.toJson(teacher);
+            return gson.toJson("Teacher updated");
         });
 
         delete("/api/v1/teachers/:id", (req, res) -> {
             Gson gson = new Gson();
             int id = Integer.parseInt(req.params(":id"));
-            Teacher teacher = teacherDao.deleteTeacher(id);
+            teacherDao.deleteTeacher(id);
             res.type("application/json");
-            return gson.toJson(teacher);
+            return gson.toJson("Teacher deleted");
         });
 
         //TODO: add student routes
         post("/api/v1/students/register", (req, res) -> {
             Gson gson = new Gson();
             Student studentData = gson.fromJson(req.body(), Student.class);
-            Student student = studentDao.createStudent(studentData);
+            studentDao.createStudent(studentData);
             res.type("application/json");
-            return gson.toJson(student);
+            return gson.toJson("Student created");
         });
 
         get("/api/v1/students/:id", (req, res) -> {
@@ -139,26 +131,26 @@ public class Router {
             studentData.setStudentId(Integer.parseInt(req.params(":id")));
             studentData.setStudentName(req.params(":studentName"));
             studentData.setSchool(req.params(":school"));
-            Student student = studentDao.updateStudent(studentData);
+            studentDao.updateStudent(studentData);
             res.type("application/json");
-            return gson.toJson(student);
+            return gson.toJson("Student updated");
         });
 
         delete("/api/v1/students/:id", (req, res) -> {
             Gson gson = new Gson();
             int id = Integer.parseInt(req.params(":id"));
-            Student student = studentDao.deleteStudent(id);
+            studentDao.deleteStudent(id);
             res.type("application/json");
-            return gson.toJson(student);
+            return gson.toJson("Student deleted");
         });
 
         //TODO: add school routes
         post("/api/v1/schools/register", (req, res) -> {
             Gson gson = new Gson();
             School schoolData = gson.fromJson(req.body(), School.class);
-            School school = schoolDao.createSchool(schoolData);
+            schoolDao.createSchool(schoolData);
             res.type("application/json");
-            return gson.toJson(school);
+            return gson.toJson("School created");
         });
 
         get("/api/v1/schools/:id", (req, res) -> {
@@ -180,19 +172,59 @@ public class Router {
             Gson gson = new Gson();
             School schoolData = gson.fromJson(req.body(), School.class);
             schoolData.setId(Integer.parseInt(req.params(":id")));
-            School school = schoolDao.updateSchool(schoolData);
+            schoolDao.updateSchool(schoolData);
             res.type("application/json");
-            return gson.toJson(school);
+            return gson.toJson("School updated");
         });
 
         delete("/api/v1/schools/:id", (req, res) -> {
             Gson gson = new Gson();
             int id = Integer.parseInt(req.params(":id"));
-            School school = schoolDao.deleteSchool(id);
+            schoolDao.deleteSchool(id);
             res.type("application/json");
-            return gson.toJson(school);
+            return gson.toJson("School deleted");
         });
 
+        //TODO: add course routes
+        post("/api/v1/courses/register", (req, res) -> {
+            Gson gson = new Gson();
+            Subject subjectData = gson.fromJson(req.body(), Subject.class);
+            subjectDao.create(subjectData);
+            res.type("application/json");
+            return gson.toJson("Course created");
+        });
+
+        get("/api/v1/courses/:id", (req, res) -> {
+            Gson gson = new Gson();
+            int id = Integer.parseInt(req.params(":id"));
+            Subject course = subjectDao.read(id);
+            res.type("application/json");
+            return gson.toJson(course);
+        });
+
+        get("/api/v1/courses", (req, res) -> {
+            Gson gson = new Gson();
+            List<Subject> courses = subjectDao.readAll();
+            res.type("application/json");
+            return gson.toJson(courses);
+        });
+
+        patch("/api/v1/courses/:id", (req, res) -> {
+            Gson gson = new Gson();
+            Subject subjectData = gson.fromJson(req.body(), Subject.class);
+            subjectData.setId(Integer.parseInt(req.params(":id")));
+            subjectDao.update(subjectData);
+            res.type("application/json");
+            return gson.toJson("Course updated");
+        });
+
+        delete("/api/v1/courses/:id", (req, res) -> {
+            Gson gson = new Gson();
+            int id = Integer.parseInt(req.params(":id"));
+            subjectDao.delete(id);
+            res.type("application/json");
+            return gson.toJson("Course deleted");
+        });
 
     }
 }
