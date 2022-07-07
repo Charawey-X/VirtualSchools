841bc23e09ef1335c78b97d208efd52887028f18
bryanbill <emobrya@gmail.com>
2022-07-07 11:49:06 +0300

    ft:Util -Adds password hasher

---
+++ src/main/java/org/example/utils/Hasher.java
@@ -0,0 +1,23 @@
+package org.example.utils;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+
+public class Hasher {
+    public static String hash(String password) {
+        try{
+            SecureRandom random = new SecureRandom();
+            byte[] salt = new byte[16];
+            random.nextBytes(salt);
+
+            KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            byte[] hash = factory.generateSecret(spec).getEncoded();
+            return hash.toString();
+        }catch (Exception e){
+            return password;
+        }
+    }
+}
